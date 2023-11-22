68f82617e9735ce484c5327ccbae74fc6b4884ec
Ocean Yeung <oceanyeung123@gmail.com>
2023-11-22 17:47:44 +0800

    A program to clean data and compose

--- dataCawler/composeData.py
+++ dataCawler/composeData.py
@@ -1,4 +1,11 @@
 import json
+from tqdm import tqdm
+
+def remove_special_characters(input_string):
+    special_chars = ['\u00ad', '\u2018', '\u2019', '\u201c', '\u201d', '\u00AD', '\n', '\t', '\r', '\f']
+    for char in special_chars:
+        input_string = input_string.replace(char, "")
+    return input_string
 
 with open('./newsData/nbcnewsData.json', 'r') as f:
     nbcnewsData = json.load(f)
@@ -14,7 +21,23 @@ print('The Standar:', len(theStandardData))
 print('Aljazeeray:', len(aljazeerayData))
 
 newData = nbcnewsData + theStandardData + aljazeerayData
-print('Total news:', len(newData))
+category_counts = {}
 
-with open('../data/newsData.json', 'w') as f:
-    json.dump(newData, f)
+for data in tqdm(newData, desc="Cleaning", unit="item"):
+    data['title'] = remove_special_characters(data['title'])
+    data['content'] = remove_special_characters(data['content'])
+
+    category = data['category']
+    if category in category_counts:
+        category_counts[category] += 1
+    else:
+        category_counts[category] = 1
+
+with open('../data/newsData.json', 'w', encoding = 'utf-8') as f:
+    json.dump(newData, f, indent = 4)
+
+for category, count in category_counts.items():
+    print(f"Category: {category}, Count: {count}")
+
+
+
