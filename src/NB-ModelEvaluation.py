f0add9b01fc0d911f9cca54e32a4a9445ff2446d
Ocean Yeung <oceanyeung123@gmail.com>
2023-11-23 17:44:19 +0800

    Add text output

--- src/NB-ModelEvaluation.py
+++ src/NB-ModelEvaluation.py
@@ -1,7 +1,7 @@
-import pandas as pd  # 導入pandas庫，用於數據操作
-from sklearn.model_selection import train_test_split  # 從scikit-learn導入用於分割數據的工具
-from sklearn.naive_bayes import MultinomialNB  # 導入多項式朴素貝葉斯分類器
-from sklearn.metrics import classification_report, accuracy_score  # 導入評估模型性能的工具
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, accuracy_score
 import config.Config as config
 from joblib import dump
 
@@ -10,18 +10,33 @@ df = pd.read_json(config.dataSetPath)
 df["text"] = df["title"] + ". " + df["content"]
 print(len(df), 'data loaded from dataset')
 
+# Vectorize news data
 tfidf = config.tfidf
-X = tfidf.fit_transform(df['text'])  # 將文本數據轉換為TF-IDF特徵
-dump(tfidf, config.vectorizerPath) # 儲存tfidf向量器
-y = df['category']  # 設置目標變量為‘category’列
+print('Vectorizing...')
+X = tfidf.fit_transform(df['text'])
+print('Vectorized')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=40)  # 分割數據為訓練集和測試集
+# Save vectorizer
+print('Saving vectorizer...')
+dump(tfidf, config.vectorizerPath)
+print('Vectorizer saved')
 
-model = MultinomialNB()  # 初始化多項式朴素貝葉斯模型
-model.fit(X_train, y_train)  # 使用訓練數據訓練模型
+y = df['category']
+# split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=40)
 
-y_pred = model.predict(X_test)  # 使用模型進行預測
-print(accuracy_score(y_test, y_pred))  # 計算並打印準確度
-print(classification_report(y_test, y_pred))  # 輸出分類報告
+model = MultinomialNB()
+print('Training model...')
+model.fit(X_train, y_train)
+print('Model trained')
 
-dump(model, './model/NB-Model.joblib')
+# Save model
+print('Saving model...')
+dump(model, config.nb_classificationModelPath)
+print('Model saved')
+
+# make predictions for test data and print the classification report
+y_pred = model.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
