38ed43feb71ebb2687302a6b9ea587d9ca7c838b
Ocean Yeung <oceanyeung123@gmail.com>
2023-11-26 22:04:14 +0800

    Update config

--- src/config/Config.py
+++ src/config/Config.py
@@ -6,17 +6,13 @@ dataSetPath = '../data/newsDataSet.json'
 
 #tfidf
 stopWordListPath = '../data/stopWordList.csv'
-max_features = 5000
-token_pattern = r'\b[a-zA-Z]{2,}\b'
 tfidf = TfidfVectorizer(
-    max_features=max_features,
+    max_features=5000,
     stop_words=list(ENGLISH_STOP_WORDS.union(set(pd.read_csv(stopWordListPath)['stop_word']))),
-    token_pattern=token_pattern
+    token_pattern=r'\b[a-zA-Z]{2,}\b'
 )
 tfidf = TfidfVectorizer()
 vectorizerPath = './model/tfidf_vectorizer.joblib'
 
-# model
+# NB model
 nb_classificationModelPath = './model/NB-Model.joblib'
-
-apiKey = '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'
