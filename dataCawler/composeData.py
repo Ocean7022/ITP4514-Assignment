242d1b1e6ae286e57850930c7306da39582fb9d1
Ocean Yeung <oceanyeung123@gmail.com>
2023-11-27 17:01:08 +0800

    Add cut file function

--- dataCawler/composeData.py
+++ dataCawler/composeData.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 from tqdm import tqdm
 
 def remove_special_characters(input_string):
@@ -7,23 +7,46 @@ def remove_special_characters(input_string):
         input_string = input_string.replace(char, "")
     return input_string
 
+def cutFile(path, part = 3):
+    with open(path, 'r') as f:
+        data = json.load(f)
+
+    fileName = os.path.basename(path)
+    numOfEachPart = int(len(data) / part)
+
+    for i in range(part):
+        if i == part - 1:
+            partData = data[i*numOfEachPart:]
+        else:
+            partData = data[i*numOfEachPart:(i + 1)*numOfEachPart]
+
+        with open(f'{path[:-len(fileName)]}{fileName[:-5]}-part{i + 1}.json', 'w') as f:
+            json.dump(partData, f, indent=4)
+
 with open('./newsData/nbcnewsData.json', 'r') as f:
     nbcnewsData = json.load(f)
 
 with open('./newsData/theStandard.json', 'r') as f:
     theStandardData = json.load(f)
 
-with open('./newsData/aljazeerayData.json', 'r') as f:
-    aljazeerayData = json.load(f)
+cutFile('./newsData/aljazeerayData.json')
+aljazeerayData = []
+for i in range(3):
+    with open(f'./newsData/aljazeerayData-part{i + 1}.json', 'r') as f:
+        aljazeerayData += json.load(f)
+
+with open('./newsData/cnnnewsData.json', 'r') as f:
+    cnnnewsData = json.load(f)
 
 print('NBC:', len(nbcnewsData))
 print('The Standar:', len(theStandardData))
 print('Aljazeeray:', len(aljazeerayData))
+print('CNN:', len(cnnnewsData))
 
-newData = nbcnewsData + theStandardData + aljazeerayData
+newData = nbcnewsData + theStandardData + aljazeerayData + cnnnewsData
 category_counts = {}
 
-for data in tqdm(newData, desc="Cleaning", unit="item"):
+for data in tqdm(newData, desc="Cleaning", unit="item", ncols=80):
     data['title'] = remove_special_characters(data['title'])
     data['content'] = remove_special_characters(data['content'])
 
@@ -37,7 +60,6 @@ with open('../data/newsDataSet.json', 'w', encoding = 'utf-8') as f:
     json.dump(newData, f, indent = 4)
 
 for category, count in category_counts.items():
-    print(f"Category: {category}, Count: {count}")
+    print(f"{category:<{10}} - Count: {count}")
 
-
-
+cutFile('../data/newsDataSet.json', 4)
