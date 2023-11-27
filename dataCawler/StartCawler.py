6ba0be1719140940e3a6bfd7d95ae055724b1ad0
Ocean Yeung <oceanyeung123@gmail.com>
2023-11-27 17:01:20 +0800

    Save

--- dataCawler/StartCawler.py
+++ dataCawler/StartCawler.py
@@ -1,9 +1,10 @@
-import nbcnews, theStandard, aljazeeranews
+import nbcnews, theStandard, aljazeeranews, cnnnews
 
 cawlers = [
     nbcnews.nbcnews(useJSON = False, onlyGetLinks = False),
     theStandard.theStandard(useJSON = True, onlyGetLinks = False),
-    aljazeeranews.aljazeeranews(useJSON = True, onlyGetLinks = False)
+    aljazeeranews.aljazeeranews(useJSON = True, onlyGetLinks = False),
+    cnnnews.cnnnews(useJSON = True, onlyGetLinks = False)
 ]
 
 for cawler in cawlers:
