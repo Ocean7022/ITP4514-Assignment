fe894ddfba2c131ae20fd5284f5a35c0954dad70
Ocean7022 <oceanyeung123@gmail.com>
2023-12-02 01:57:00 +0900

    Update README.md

--- src/config/Config.py
+++ src/config/Config.py
@@ -30,16 +30,16 @@ RNNDataSetPath = dataSetPath01
 stopWordList = ENGLISH_STOP_WORDS.union(set(pd.read_csv(stopWordListPath)['stop_word']))
 pattern = r'\b[a-zA-Z]{3,}\b'
 
-batch_size = 128
+batch_size = 256
 train_ratio = 0.8
 
-max_length = 1200
+max_length = 1000
 vocab_size = 14000
 embedding_dim = 400
 input_size = embedding_dim
 hidden_size = 128
 num_layers = 3
 num_classes = 9 # num of types of news
-learning_rate = 0.002
+learning_rate = 0.003
 num_epochs = 20
 
