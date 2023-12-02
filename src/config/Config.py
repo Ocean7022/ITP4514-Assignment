bc8c7eac5898d171d02ee004e94b0ff03eed0951
Ocean Yeung <oceanyeung123@gmail.com>
2023-12-03 00:07:41 +0800

    Update Config.py

--- src/config/Config.py
+++ src/config/Config.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
 
-# DataSet
-dataSetPath = '../data/newsDataSet.json'
-        
-#tfidf
 stopWordListPath = '../data/stopWordList.csv'
+
+# NB model
+dataSetPath = '../data/newsDataSet.json'
+vectorizerPath = './model/tfidf_vectorizer.joblib'
+nb_classificationModelPath = './model/NB-ClassificationModel.joblib'
+resultPhotoPath = '../img/NB-Tuning/Result.png'
 tfidf = TfidfVectorizer(
     max_features=8000,
     ngram_range=(1, 3),
@@ -16,12 +18,6 @@ tfidf = TfidfVectorizer(
     norm='l2',
     sublinear_tf=True
 )
-#tfidf = TfidfVectorizer()
-vectorizerPath = './model/tfidf_vectorizer.joblib'
-# NB model
-nb_classificationModelPath = './model/NB-ClassificationModel.joblib'
-
-
 
 # GUR model
 GRUDataSetPath = '../data/GRUDataSet.pth'
@@ -33,10 +29,13 @@ pattern = r'\b[a-zA-Z]{3,}\b'
 batch_size = 128
 train_ratio = 0.8
 
+# Data preprocessing parameters
 max_length = 1000
 vocab_size = 14000
 embedding_dim = 400
 input_size = embedding_dim
+
+# Training model parameters
 hidden_size = 128
 num_layers = 3
 num_classes = 9 # num of types of news
