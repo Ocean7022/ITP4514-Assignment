14b4d34397125ff171991a84554f024f87f269c7
Ocean Yeung <oceanyeung123@gmail.com>
2023-12-03 00:10:32 +0800

    Update FormGenerator.py

--- FormGenerator.py
+++ FormGenerator.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-import matplotlib.pyplot as plt
-
 categories = ['business', 'culture', 'education', 'health', 'politics', 'property', 'sport', 'technology', 'travel']
 precisions = [0.90, 0.90, 0.88, 0.83, 0.89, 0.78, 0.99, 0.86, 0.64]
 recalls = [0.95, 0.67, 0.68, 0.90, 0.94, 0.52, 0.98, 0.55, 0.84]
@@ -12,7 +10,7 @@ f1_scores = [0.93, 0.77, 0.76, 0.86, 0.92, 0.62, 0.98, 0.67, 0.72]
 bar_width = 0.25
 index = range(len(categories))
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(11, 7))
 bar1 = ax.bar([i - bar_width for i in index], precisions, width=bar_width, label='Precision')
 bar2 = ax.bar(index, recalls, width=bar_width, label='Recall')
 bar3 = ax.bar([i + bar_width for i in index], f1_scores, width=bar_width, label='F1-Score')
@@ -23,6 +21,8 @@ ax.set_title('Precision, Recall and F1-Score for Each Category')
 ax.set_xticks([i for i in index])
 ax.set_xticklabels(categories, rotation=45)
 ax.legend()
+
+plt.subplots_adjust(bottom=0.17, top=0.94, left=0.06, right=0.99)
 
 plt.show()
 exit()
