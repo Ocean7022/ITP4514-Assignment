dcafbdc70ddcd0e4bfad2ed776462a057ff1fd83
Ocean Yeung <oceanyeung123@gmail.com>
2023-12-03 15:39:31 +0800

    Update NB_FeatureAnalysis

--- src/NB_FeatureAnalysis.py
+++ src/NB_FeatureAnalysis.py
@@ -3,26 +3,41 @@ from tqdm import tqdm
 import pandas as pd
 import config.Config as config
 
-print('Loading dataset...')
-df = pd.read_json(config.dataSetPath)
-df["text"] = df["title"] + ". " + df["content"]
-print(len(df), 'data loaded from dataset')
+class NB_FeatureAnalysis:
+    def __init__(self):
+        self.numOfFeatures = input('\nHow many features do you want to output for each category? (10 - 50): ')
+        if self.numOfFeatures.isdigit():
+            self.numOfFeatures = int(self.numOfFeatures)
+            if self.numOfFeatures < 10 or self.numOfFeatures > 50:
+                print('Invalid input, please try again.\n')
+                self.__init__()
+        else:
+            print('Invalid input, please try again.\n')
+            self.__init__()
 
-tfidf = config.tfidf
-features_per_category = {}
+        self.__analyze()
 
-for category in tqdm(
-    df["category"].unique(), desc="Calculating TF-IDF", unit=" category"
-):
-    category_docs = df[df["category"] == category]["text"]
-    tfidf_matrix = tfidf.fit_transform(category_docs)
-    mean_scores = np.mean(tfidf_matrix, axis=0).A1
-    features = tfidf.get_feature_names_out()
-    scores_with_features = list(zip(mean_scores, features))
-    sorted_scores = sorted(scores_with_features, reverse=True)
-    features_per_category[category] = sorted_scores[:10]
+    def __analyze(self):
+        print('Loading dataset...')
+        df = pd.read_json(config.dataSetPath)
+        df["text"] = df["title"] + ". " + df["content"]
+        print(len(df), 'data loaded from dataset')
 
-for category in features_per_category:
-    print(f"\nCategory: {category}")
-    for score, feature in features_per_category[category]:
-        print(f"{feature}: {score}")
+        tfidf = config.tfidf
+        features_per_category = {}
+
+        for category in tqdm(
+            df["category"].unique(), desc="Calculating TF-IDF", unit=" category"
+        ):
+            category_docs = df[df["category"] == category]["text"]
+            tfidf_matrix = tfidf.fit_transform(category_docs)
+            mean_scores = np.mean(tfidf_matrix, axis=0).A1
+            features = tfidf.get_feature_names_out()
+            scores_with_features = list(zip(mean_scores, features))
+            sorted_scores = sorted(scores_with_features, reverse=True)
+            features_per_category[category] = sorted_scores[:self.numOfFeatures]
+
+        for category in features_per_category:
+            print(f"\nCategory: {category}")
+            for score, feature in features_per_category[category]:
+                print(f"{feature}: {score}")
--- src/Start.py
+++ src/Start.py
@@ -6,45 +6,46 @@ class Start:
             print('\nITP4514 Assignment - News Classification and Summarization')
             print('------ Models ------')
             print(' - Classification - ')
-            print('[0] - Naive Bayes (NB)')
-            print('[1] - Gated Recurrent Unit (GRU)')
+            print('[1] - Naive Bayes (NB)')
+            print('[2] - Gated Recurrent Unit (GRU)')
             print(' - Summerization - ')
-            print('[2] - Bidirectional Encoder Representations from Transformers (BERT)')
-            print('[3] - T5')
-            print('\n[4] - Exit')
+            print('[3] - Bidirectional Encoder Representations from Transformers (BERT)')
+            print('[4] - T5')
+            print('\n[x] - Exit')
             option = input('Select a model: ')
-            if option == '0':
+            if option == '1':
                 self.__nb()
-            elif option == '1':
-                self.__gru()
             elif option == '2':
-                self.__bert()
+                self.__gru()
             elif option == '3':
-                self.__t5()
+                self.__bert()
             elif option == '4':
+                self.__t5()
+            elif option == 'x':
                 exit()
             else:
                 print('Invalid input, please try again.\n')
 
     def __nb(self):
         print('\n - Naive Bayes (NB) - ')
-        print('[0] - Start Classification')
+        print('[1] - Start Classification')
         print('      Classify the new(s).')
-        print('[1] - Start Model Evaluation')
+        print('[2] - Start Model Evaluation')
         print('      Train and test the model.')
-        print('[2] - Start Feature Analysis') 
+        print('[3] - Start Feature Analysis') 
         print('      Output the features of each category.')
-        print('\n[3] - Back')
+        print('\n[b] - Back')
         option = input('Select an option: ')
-        if option == '0':
+        if option == '1':
             import NB_Classification as NB
-        elif option == '1':
+        elif option == '2':
             import NB_ModelEvaluation as NB
             NB.NB_ModelEvaluation()
-        elif option == '2':
-            pass
         elif option == '3':
-            return True
+            import NB_FeatureAnalysis as NB
+            NB.NB_FeatureAnalysis()
+        elif option == 'b':
+            Start()
         else:
             print('Invalid input, please try again.\n')
             self.__nb()
